643a5ee8618ab5727aa07d0409290b74b5b27c22
lixunlv <lxunlv@163.com>
2015-11-24 15:47:00 +0800

    add route2

--- components/OrderItem.js
+++ components/OrderItem.js
@@ -1,29 +1,26 @@
 import React, { PropTypes, Component } from 'react'
-import { Router, Route, Link, Redirect } from 'react-router'
+import { Link } from 'react-router'
 
 export default class OrderItem extends Component {
     render() {
         const {sn, name, price, ref} = this.props.order;
         return (
-            <div className="weui_cell flex_box" style={{cursor: 'pointer'}} >
-                    <div className="weui_cell_hd column_cell">
-                        <Link to={`/order/${sn}`} >
-                            { (ref == true ? 'TH' : 'XS') + sn }
-                        </Link>
-                    </div>
+            <Link to={`/order/${sn}`} className="weui_cell flex_box" style={{cursor: 'pointer'}}>
+                <div className="weui_cell_hd column_cell">
+                    { (ref == true ? 'TH' : 'XS') + sn }
+                </div>
 
                 <div className="weui_cell_hd column_cell"
                      style={{flexBasis:0, overflow: 'hidden', textOverflow: 'ellipsis'}}> {name} </div>
                 <div className="weui_cell_ft column_cell">
                     {price}
                 </div>
-            </div>
+            </Link>
         )
     }
 }
 
 OrderItem.propTypes = {
     order: PropTypes.object.isRequired,
-    //onSnClicked: PropTypes.func.isRequired
 }
 
